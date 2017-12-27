333a09fd8a1769a3ed08afd2182e848991f22a15
partizand <elegaunte@gmail.com>
2017-12-27 10:25:31 +0300

    delete omv keys script

--- usr/share/openmediavault/confdb/delete.d/conf.service.autoshutdown.sh
+++ usr/share/openmediavault/confdb/delete.d/conf.service.autoshutdown.sh
@@ -4,7 +4,7 @@ set -e
 
 . /usr/share/openmediavault/scripts/helper-functions
 
-SERVICE_XPATH_NAME="letsencrypt"
+SERVICE_XPATH_NAME="autoshutdown"
 SERVICE_XPATH="/config/services/${SERVICE_XPATH_NAME}"
 
 if omv_config_exists "${SERVICE_XPATH}"; then
