8707915e55eece92be57905339df3aec182141f5
Philipp S. aka ErdnussFlipS <erdnussflips93@gmail.com>
2018-04-08 22:49:48 +0200

    Updated plugin system readme and added disabled example plugin file

---
+++ etc/autoshutdown.d/scripts.d/example.plugin-disabled.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "--------------------------------------------------"
+echo "This is an example plugin"
+echo "It won't be prevent your system from shutting down"
+echo "This script is disabled by default. To enabled it rename it to 'example.plugin.sh'"
+
+echo "The autoshutdown configuration will be autmatically loaded by the plugin system"
+echo "All variables from /etc/autoshutdown.conf are available, see 'declare -p'"
+
+echo "Output of 'declare -p'"
+declare -p
+
+echo "At the end, we will exit with exitcode 0"
+echo "--------------------------------------------------"
+
+exit 0
