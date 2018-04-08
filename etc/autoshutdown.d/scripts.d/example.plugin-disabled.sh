959223df05d0c9feada4ad6dd9fcfa9e9ef27ef1
Philipp S. aka ErdnussFlipS <erdnussflips93@gmail.com>
2018-04-08 22:50:41 +0200

    Fixed typo

--- etc/autoshutdown.d/scripts.d/example.plugin-disabled.sh
+++ etc/autoshutdown.d/scripts.d/example.plugin-disabled.sh
@@ -5,7 +5,7 @@ echo "This is an example plugin"
 echo "It won't be prevent your system from shutting down"
 echo "This script is disabled by default. To enabled it rename it to 'example.plugin.sh'"
 
-echo "The autoshutdown configuration will be autmatically loaded by the plugin system"
+echo "The autoshutdown configuration will be automatically loaded by the plugin system"
 echo "All variables from /etc/autoshutdown.conf are available, see 'declare -p'"
 
 echo "Output of 'declare -p'"
