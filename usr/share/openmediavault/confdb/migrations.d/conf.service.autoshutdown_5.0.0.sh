0a9e2ae3efcae047c4d18ad391267c93698a803f
Aaron Murray <plugins@omv-extras.org>
2020-03-02 14:00:42 -0600

    add 5.0 migration script

---
+++ usr/share/openmediavault/confdb/migrations.d/conf.service.autoshutdown_5.0.0.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+# @author    OpenMediaVault Plugin Developers <plugins@omv-extras.org>
+# @copyright Copyright (c) 2020 OpenMediaVault Plugin Developers
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+. /usr/share/openmediavault/scripts/helper-functions
+
+SERVICE_XPATH_NAME="autoshutdown"
+SERVICE_XPATH="/config/services/${SERVICE_XPATH_NAME}"
+
+omv_config_rename "${SERVICE_XPATH}/uphours-begin" "uphours_begin"
+omv_config_rename "${SERVICE_XPATH}/uphours-end" "uphours_end"
+
+exit 0
