bab8d9b1ff2ebd046ad7b14df00662423d284adc
mnul <junk@pixelemotionen.de>
2021-05-07 14:01:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -71,7 +71,7 @@ sudo cp -R lib/* /lib/
 sudo cp autoshutdown.service /etc/systemd/system/autoshutdown.service
 
 # check if there is an existing autoshutdown.conf if so the skip copying.
-if [[ -f "/etc/autoshutdown.conf"]];
+if [[ -f "/etc/autoshutdown.conf"]]
   then
     echo "" && echo "" && echo ""
     echo "configuration file exists, skipping creation of default configuration."
@@ -97,7 +97,7 @@ sudo systemctl daemon-reload
 echo "***********************  DONE! ***********************************************"
 echo "" && echo "" && echo ""
 echo "" && echo "" && echo ""
-if [[ $EXISTCONF -eq 1]];
+if [[ $EXISTCONF -eq 1]]
   then
     echo "Autoshutdown is updated to the latest version."
     echo "" && echo ""
